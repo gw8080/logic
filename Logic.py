c645e569dd865214734fb5c67b873f38d4614df9
gw8080 <52276914+gw8080@users.noreply.github.com>
2022-02-26 15:33:03 +1100

    Add files via upload

--- Logic.py
+++ Logic.py
@@ -4,6 +4,9 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import wordnet as wn
 import random
 f = open('noun.txt',"r")
+fa = open("logic_output.txt", "w")
+
+
 for Object in f:
     proc = wn.synsets(Object.strip("\n"))
     if proc:
@@ -15,12 +18,25 @@ for Object in f:
                 if pluralize(proc[0].name().split(".")[0]) != pluralize(proc3[0].name().split(".")[0]):
                     many = pluralize(proc[0].name().split(".")[0]) + " are " + pluralize(proc3[0].name().split(".")[0])
                     rand = random.choice(proc).name().split(".")[0]
-                    proc5 = wn.synsets(proc4, pos=wn.NOUN)[0].name().split(".")[0]
-                    if rand != proc5:
-                        second = rand + " is a " + proc5
-                        final = pluralize(rand) + " are " + pluralize(proc3[0].name().split(".")[0])
-                        print(many)
-                        print(second)
-                        print(final)
-                        print()
+                    if wn.synsets(proc4, pos=wn.NOUN):
+                        proc5 = wn.synsets(proc4, pos=wn.NOUN)[0].name().split(".")[0]
+                        if rand != proc5:
+                            second = rand + " is a " + proc5
+                            final = pluralize(rand) + " are " + pluralize(proc3[0].name().split(".")[0])
+                            print(many)
+                            print(second)
+                            print(final)
+                            print()
+                            fa.write(many + "\n")
+                            fa.write(second + "\n")
+                            fa.write(final + "\n")
+                            fa.write("\n")
+                            fa.flush()
+fa.close()
 #prove statement with a syllogism
+
+
+
+
+
+
