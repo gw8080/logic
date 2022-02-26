01f0c736471d8df492dd321cdb4658a787caa6d5
gw8080 <52276914+gw8080@users.noreply.github.com>
2022-02-26 21:56:34 +1100

    Add files via upload

--- Logic.py
+++ Logic.py
@@ -11,30 +11,31 @@ for Object in f:
     if proc:
         for item in proc:
             proc2 = item.name().split(".")[0]
-            proc3 = item.hypernyms()
+            proc3 = item.hyponyms()
             if proc3:
-                proc4 = proc[0].name().split(".")[0]
-                if pluralize(proc[0].name().split(".")[0]) != pluralize(proc3[0].name().split(".")[0]):
-                    many = pluralize(proc[0].name().split(".")[0]) + " are " + pluralize(proc3[0].name().split(".")[0])
-                    rand = random.choice(proc).name().split(".")[0]
-                    if wn.synsets(singularize(proc3[0].name().split(".")[0]), pos=wn.VERB):
-                        if wn.synsets(singularize(proc3[0].name().split(".")[0]), pos=wn.VERB)[0].name().split(".")[0] != singularize(proc3[0].name().split(".")[0]):
-                            must = "you should " + wn.synsets(singularize(proc3[0].name().split(".")[0]), pos=wn.VERB)[0].name().split(".")[0] + " " +  singularize(proc[0].name().split(".")[0])+ " " + singularize(proc3[0].name().split(".")[0])  + " for " + rand
-                            if wn.synsets(proc4, pos=wn.NOUN):
-                                proc5 = wn.synsets(proc4, pos=wn.NOUN)[0].name().split(".")[0]
-                                if rand != proc5:
-                                    second = rand + " is a " + proc5
-                                    final = pluralize(rand) + " are " + pluralize(proc3[0].name().split(".")[0])
-                                    print(many)
-                                    print(second)
-                                    print(final)
-                                    print(must)
-                                    print()
-                                    fa.write(many + "\n")
-                                    fa.write(second + "\n")
-                                    fa.write(final + "\n")
-                                    fa.write(must + "\n\n")
-                                    fa.flush()
+                for itemB in proc3:
+                    proc4 = item.name().split(".")[0]
+                    if pluralize(item.name().split(".")[0]) != pluralize(itemB.name().split(".")[0]):
+                        many = pluralize(item.name().split(".")[0]) + " are " + pluralize(itemB.name().split(".")[0])
+                        rand = random.choice(proc).name().split(".")[0]
+                        if wn.synsets(singularize(itemB.name().split(".")[0]), pos=wn.VERB):
+                            if wn.synsets(singularize(itemB.name().split(".")[0]), pos=wn.VERB)[0].name().split(".")[0] != singularize(itemB.name().split(".")[0]):
+                                must = "you should " + wn.synsets(singularize(itemB.name().split(".")[0]), pos=wn.VERB)[0].name().split(".")[0] + " " +  singularize(proc[0].name().split(".")[0])+ " " + singularize(itemB.name().split(".")[0])  + " for " + rand
+                                if wn.synsets(proc4, pos=wn.NOUN):
+                                    proc5 = wn.synsets(proc4, pos=wn.NOUN)[0].name().split(".")[0]
+                                    if rand != proc5:
+                                        second = rand + " is a " + proc5
+                                        final = pluralize(rand) + " are " + pluralize(itemB.name().split(".")[0])
+                                        print(many)
+                                        print(second)
+                                        print(final)
+                                        print(must)
+                                        print()
+                                        fa.write(many + "\n")
+                                        fa.write(second + "\n")
+                                        fa.write(final + "\n")
+                                        fa.write(must + "\n\n")
+                                        fa.flush()
 fa.close()
 #prove statement with a syllogism
 
