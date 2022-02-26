11e813abd4af9a095a41d2a3cd9c4715dc2d5441
gw8080 <52276914+gw8080@users.noreply.github.com>
2022-02-26 18:38:15 +1100

    Add files via upload

--- Logic.py
+++ Logic.py
@@ -1,22 +1,12 @@
-    #SynthReason - Synthetic dawn
-    #Copyright (C) 2022 George Wagenknecht
-    #This program is free software: you can redistribute it and/or modify
-    #it under the terms of the GNU General Public License as published by
-    #the Free Software Foundation, either version 3 of the License, or
-    #(at your option) any later version.
-    #This program is distributed in the hope that it will be useful,
-    #but WITHOUT ANY WARRANTY; without even the implied warranty of
-    #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    #GNU General Public License for more details.
-    #You should have received a copy of the GNU General Public License
-    #along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 #Count of object expression -> success/requirement expression -> action-object subset non/commonality expression = Generalisation.
 from pattern.en import pluralize, singularize
+from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import wordnet as wn
 import random
+from operator import itemgetter
 f = open('objects.txt',"r")
 fa = open("logic_output.txt", "w")
+directive = open("directive_output.txt", "w")
 for Object in f:
     proc = wn.synsets(Object.strip("\n"))
     if proc:
@@ -28,6 +18,8 @@ for Object in f:
                 if pluralize(proc[0].name().split(".")[0]) != pluralize(proc3[0].name().split(".")[0]):
                     many = pluralize(proc[0].name().split(".")[0]) + " are " + pluralize(proc3[0].name().split(".")[0])
                     rand = random.choice(proc).name().split(".")[0]
+                    must = "one must achieve " + singularize(proc3[0].name().split(".")[0]) + " " + singularize(proc[0].name().split(".")[0]) + " for " + rand
+                    mustB = "you require " + singularize(proc[0].name().split(".")[0]) + " for " + rand + " " +  singularize(proc3[0].name().split(".")[0])
                     if wn.synsets(proc4, pos=wn.NOUN):
                         proc5 = wn.synsets(proc4, pos=wn.NOUN)[0].name().split(".")[0]
                         if rand != proc5:
@@ -36,10 +28,15 @@ for Object in f:
                             print(many)
                             print(second)
                             print(final)
+                            print(must)
+                            print(mustB)
                             print()
                             fa.write(many + "\n")
                             fa.write(second + "\n")
                             fa.write(final + "\n")
+                            directive.write(must + "\n")
+                            directive.write(mustB + "\n\n")
+                            directive.flush()
                             fa.write("\n")
                             fa.flush()
 fa.close()
